b427e93546a27d305b9fe348412da9f0be3c5c78
Tim Bezhashvyly <tim@openstream.ch>
2011-12-15 21:06:49 +0100

    Removed Magento URLs from configuration, added SaferPay URLs into configuration.

--- app/code/community/Saferpay/Ecommerce/sql/ecommerce_setup/mysql4-install-0.0.1.php
+++ app/code/community/Saferpay/Ecommerce/sql/ecommerce_setup/mysql4-install-0.0.1.php
@@ -1,12 +1,19 @@
 <?php
 
- $installer = $this;
- $request = Mage::app()->getRequest();
- $url = $request->getScheme() . '://' . $request->getHttpHost();
- 
- $installer->setConfigData('saferpay/settings/success_url', $url.'/saferpay/process/success/');
- $installer->setConfigData('saferpay/settings/back_url', $url.'/saferpay/process/back/');
- $installer->setConfigData('saferpay/settings/fail_url', $url.'/saferpay/process/fail/');
- $installer->setConfigData('saferpay/settings/notify_url', $url.'/saferpay/process/notify/');
+$installer = $this;
+
+$statusTable        = $installer->getTable('sales/order_status');
+$statusStateTable   = $installer->getTable('sales/order_status_state');
+$statusLabelTable   = $installer->getTable('sales/order_status_label');
+
+$data = array(
+	array('status' => 'authorized', 'label' => 'Authorized Payment')
+);
+$installer->getConnection()->insertArray($statusTable, array('status', 'label'), $data);
+
+$data = array(
+	array('status' => 'authorized', 'state' => 'authorized', 'is_default' => 1)
+);
+$installer->getConnection()->insertArray($statusStateTable, array('status', 'state', 'is_default'), $data);
 
 ?>
