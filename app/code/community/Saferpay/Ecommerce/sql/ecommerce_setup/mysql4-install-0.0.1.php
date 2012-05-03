c88005a5013bed699c2ece52bb33c662d2d58522
Tim Bezhashvyly <tim@openstream.ch>
2012-05-03 15:34:26 +0200

    1.0.4 - Backward compatibility patch.

--- app/code/community/Saferpay/Ecommerce/sql/ecommerce_setup/mysql4-install-0.0.1.php
+++ app/code/community/Saferpay/Ecommerce/sql/ecommerce_setup/mysql4-install-0.0.1.php
@@ -1,19 +1,27 @@
 <?php
 
-$installer = $this;
+	$magentoVersion = Mage::getVersionInfo();
+	if($magentoVersion['major'] < 2 && $magentoVersion['minor'] < 5){
 
-$statusTable        = $installer->getTable('sales/order_status');
-$statusStateTable   = $installer->getTable('sales/order_status_state');
-$statusLabelTable   = $installer->getTable('sales/order_status_label');
+		// Installer is not needed for versions older then 1.5.0.0
 
-$data = array(
-	array('status' => 'authorized', 'label' => 'Authorized Payment')
-);
-$installer->getConnection()->insertArray($statusTable, array('status', 'label'), $data);
+	}else{
 
-$data = array(
-	array('status' => 'authorized', 'state' => 'authorized', 'is_default' => 1)
-);
-$installer->getConnection()->insertArray($statusStateTable, array('status', 'state', 'is_default'), $data);
+		$installer = $this;
+
+		$statusTable        = $installer->getTable('sales/order_status');
+		$statusStateTable   = $installer->getTable('sales/order_status_state');
+		$statusLabelTable   = $installer->getTable('sales/order_status_label');
+
+		$data = array(
+			array('status' => 'authorized', 'label' => 'Authorized Payment')
+		);
+		$installer->getConnection()->insertArray($statusTable, array('status', 'label'), $data);
+
+		$data = array(
+			array('status' => 'authorized', 'state' => 'authorized', 'is_default' => 1)
+		);
+		$installer->getConnection()->insertArray($statusStateTable, array('status', 'state', 'is_default'), $data);
+	}
 
 ?>
